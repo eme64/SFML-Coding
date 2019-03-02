ea55539a4a3463358d87a06da87742b74d8c540f
Peter Emanuel <emanuel.peter@hotmail.com>
2019-03-02 00:11:18 -0800

    added mouse movement notification: basis for drag/click/...

--- 12 gui project/ep_gui.hpp
+++ 12 gui project/ep_gui.hpp
@@ -118,10 +118,10 @@ namespace EP {
       std::string name() const {return name_;}
       std::string fullName() const {if (parent_) {return parent_->fullName()+"/"+name_;}else{return name_;}}
 
-      virtual void draw(const float px,const float py,const float scale, sf::RenderTarget &target) {
-        DrawRect(x_*scale+py, y_*scale+py, dx_*scale, dy_*scale, target, Color(0.5,0.1,0.1));
+      virtual void draw(const float px,const float py, sf::RenderTarget &target) {
+        DrawRect(x_+py, y_+py, dx_, dy_, target, Color(0.5,0.1,0.1));
         for (std::list<Area*>::reverse_iterator rit=children_.rbegin(); rit!=children_.rend(); ++rit) {
-          (*rit)->draw(x_*scale+py, y_*scale+py,scale,target);
+          (*rit)->draw(x_+py, y_+py,target);
         }
       }
 
@@ -129,19 +129,39 @@ namespace EP {
         if (fillParent_&&parent_) { sizeIs(dx,dy); }
       }
 
-      virtual void onMouseOver(const float px,const float py,const float scale) {}// relative to parent
       virtual void onMouseOverStart() {}
+      virtual void onMouseOver(const float px,const float py) {}// relative to parent
       virtual void onMouseOverEnd() {}
 
-      Area* checkMouseOver(const float px,const float py,const float scale,const bool doNotify=true) {// relative to parent
-        if (px>=x_*scale and py>=y_*scale and px<=(x_+dx_)*scale and py<=(y_+dy_)*scale) {
+      virtual bool onMouseDownStart(const bool isFirstDown,const float px,const float py) {
+        std::cout << "onMouseDownStart " << fullName() << " fist:" << std::to_string(isFirstDown) << std::endl;
+        return false;
+      }
+      //isFirstDown: true if just mouseDown, false if slided from other area that did not lock drag.
+      // return: capture drag. can only capture if isFreshDown==true
+
+      virtual void onMouseDown(const bool isCaptured,const float px,const float py,float &dx, float &dy) {}
+      // px,py: relative to parent, position of mouse in last frame.
+      //dx/dy the moving the mouse is trying to do. If change (eg =0), mouse position will be changed accordingly.
+
+      virtual void onMouseDownEnd(const bool isCaptured, const bool isLastDown,const float px,const float py) {
+        std::cout << "onMouseDownEnd " << fullName() << " captured:" << std::to_string(isCaptured) << " last:" << std::to_string(isLastDown) << std::endl;
+      }
+      // isLastDown: if true indicates that mouse physically released, else only left scope bc not captured
+
+
+      // report absolute shift (drag), report relative speed time (keep mouse position), remember original position (up to Area?)
+      // lock to this element?
+
+      Area* checkMouseOver(const float px,const float py,const bool doNotify=true) {// relative to parent
+        if (px>=x_ and py>=y_ and px<=(x_+dx_) and py<=(y_+dy_)) {
           for (auto &c : children_) {
-            Area* over = c->checkMouseOver(px-x_*scale,py-y_*scale,scale,doNotify);
+            Area* over = c->checkMouseOver(px-x_,py-y_,doNotify);
             if (over!=NULL) {
               return over;
             }
           }
-          if (doNotify) {onMouseOver(px,py,scale);}
+          if (doNotify) {onMouseOver(px,py);}
           return this;
         }
         return NULL;
@@ -169,20 +189,25 @@ namespace EP {
       Button(const std::string& name,Area* const parent, const float x,const float y,const float dx,const float dy, const std::string text,const std::vector<Color> bgColors,const std::vector<Color> textColors)
       : Area(name,parent,x,y,dx,dy),text_(text),bgColors_(bgColors),textColors_(textColors){}
 
-      virtual void draw(const float px,const float py,const float scale, sf::RenderTarget &target) {
-        float gx = x_*scale+px;
-        float gy = y_*scale+py;
-        DrawRect(gx, gy, dx_*scale, dy_*scale, target, bgColors_[state_]);
-        DrawText(gx+1*scale, gy+1*scale, text_, (dy_-2)*scale, target, textColors_[state_]);
+      virtual void draw(const float px,const float py, sf::RenderTarget &target) {
+        float gx = x_+px;
+        float gy = y_+py;
+        DrawRect(gx, gy, dx_, dy_, target, bgColors_[state_]);
+        DrawText(gx+1, gy+1, text_, (dy_-2), target, textColors_[state_]);
         for (std::list<Area*>::reverse_iterator rit=children_.rbegin(); rit!=children_.rend(); ++rit) {
-          (*rit)->draw(x_*scale+py, y_*scale+py,scale,target);
+          (*rit)->draw(x_+py, y_+py,target);
         }
       }
-      virtual void onMouseOver(const float px,const float py,const float scale) {// relative to parent
+      virtual void onMouseOver(const float px,const float py) {// relative to parent
         if (state_==0) {state_=1;}
       }
       virtual void onMouseOverStart() {if (state_==0) {state_=1;}}
       virtual void onMouseOverEnd() {if (state_==1) {state_=0;}}
+
+      virtual bool onMouseDownStart(const bool isFirstDown,const float px,const float py) {
+        std::cout << "onMouseDownStart " << fullName() << " fist:" << std::to_string(isFirstDown) << std::endl;
+        return true;
+      }
 
     private:
       std::string text_;
@@ -199,13 +224,25 @@ namespace EP {
                                       );
       }
 
-      virtual void draw(const float px,const float py,const float scale, sf::RenderTarget &target) {
-        float gx = x_*scale+px;
-        float gy = y_*scale+py;
-        DrawRect(gx, gy, dx_*scale, dy_*scale, target, Color(0.1,0.5,0.1));
-        DrawText(gx+borderSize*scale, gy+borderSize*scale, title_, (headerSize-2*borderSize)*scale, target, Color(1,1,1));
+      virtual void draw(const float px,const float py, sf::RenderTarget &target) {
+        float gx = x_+px;
+        float gy = y_+py;
+        DrawRect(gx, gy, dx_, dy_, target, Color(0.1,0.5,0.1));
+        DrawText(gx+borderSize, gy+borderSize, title_, (headerSize-2*borderSize), target, Color(1,1,1));
         for (std::list<Area*>::reverse_iterator rit=children_.rbegin(); rit!=children_.rend(); ++rit) {
-          (*rit)->draw(x_*scale+py, y_*scale+py,scale,target);
+          (*rit)->draw(x_+py, y_+py,target);
+        }
+      }
+
+      virtual bool onMouseDownStart(const bool isFirstDown,const float px,const float py) {
+        std::cout << "onMouseDownStart " << fullName() << " fist:" << std::to_string(isFirstDown) << std::endl;
+        return true;
+      }
+
+      virtual void onMouseDown(const bool isCaptured,const float px,const float py,float &dx, float &dy) {
+        if (isCaptured) {
+          x_+=dx;
+          y_+=dy;
         }
       }
 
@@ -215,6 +252,161 @@ namespace EP {
       const float headerSize = 20.0;// top
     };
 
+
+    class MasterWindow {
+    public:
+      MasterWindow(const size_t dx,const size_t dy,std::string title,const bool fullScreen=false) {
+        sf::ContextSettings settings;
+        settings.antialiasingLevel = 8;
+
+        if (fullScreen) {
+          renderWindow_ = new sf::RenderWindow(sf::VideoMode(dx, dy), title, sf::Style::Fullscreen, settings);
+        } else {
+          renderWindow_ = new sf::RenderWindow(sf::VideoMode(dx, dy), title, sf::Style::Default, settings);
+        }
+
+        renderWindow_->setVerticalSyncEnabled(true);
+
+        sf::Vector2u size = renderWindow_->getSize();
+        mainArea_ = (new EP::GUI::Area("main",NULL,0,0,size.x,size.y))->fillParentIs(true);
+
+        sf::Vector2i mousepos = sf::Mouse::getPosition(*renderWindow_);
+        lastMouseX_ = mousepos.x;
+        lastMouseY_ = mousepos.y;
+      }
+      sf::RenderWindow* target() {return renderWindow_;}
+      Area* area() {return mainArea_;}
+
+      void update() {
+        sf::Event event;
+        while (renderWindow_->pollEvent(event)) {
+          switch (event.type) {
+            case sf::Event::Closed: {
+              close();
+              break;
+            }
+            case sf::Event::MouseWheelScrolled: {
+              if (event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel) {
+                int delta = event.mouseWheelScroll.delta;
+              }
+              break;
+            }
+            case sf::Event::MouseButtonPressed: {
+              if(event.mouseButton.button == sf::Mouse::Left) {
+                mouseDown_ = true;
+                if (mouseOverArea_) {
+                  mouseDownArea_ = mouseOverArea_;
+                  mouseDownCaptured_ = mouseOverArea_->onMouseDownStart(true,lastMouseX_,lastMouseY_);
+                  if (mouseDownCaptured_) {
+                    std::cout << "mouseDownEvent captured by " << mouseOverArea_->fullName() << std::endl;
+                  }
+                }
+              } else if(event.mouseButton.button == sf::Mouse::Right) {
+                // forget for now
+              }
+              break;
+            }
+            case sf::Event::MouseButtonReleased: {
+              if(event.mouseButton.button == sf::Mouse::Left) {
+                mouseDown_ = false;
+                if (mouseDownArea_) {
+                  mouseDownArea_->onMouseDownEnd(mouseDownCaptured_,true,lastMouseX_,lastMouseY_);
+                }
+                mouseDownCaptured_ = false;
+                mouseDownArea_ = NULL;
+              } else if(event.mouseButton.button == sf::Mouse::Right) {
+                // forget for now
+              }
+              break;
+            }
+            case sf::Event::MouseMoved: {
+              float mouseX = event.mouseMove.x;
+              float mouseY = event.mouseMove.y;
+              const float origMouseDX = mouseX-lastMouseX_;
+              const float origMouseDY = mouseY-lastMouseY_;
+              float mouseDX = origMouseDX;
+              float mouseDY = origMouseDY;
+
+              if (mouseDown_) {
+                // captured -> simply notify, update mouse
+                // not captured: if has area: check, if leaves: update mouse, notify leaving, possibly start notify
+                // no area: update mouse, possibly start notify after
+                if (mouseDownArea_) {
+                  mouseDownArea_->onMouseDown(mouseDownCaptured_,lastMouseX_,lastMouseY_,mouseDX,mouseDY);
+                }
+              }
+
+              lastMouseX_+=mouseDX;
+              lastMouseY_+=mouseDY;
+              if (!(origMouseDX==mouseDX && origMouseDY==mouseDY)) {
+                sf::Mouse::setPosition(sf::Vector2i(lastMouseX_,lastMouseY_),*renderWindow_);
+                std::cout << "dragged!" << std::endl;
+              }
+
+              EP::GUI::Area* mouseOverNew = mainArea_->checkMouseOver(lastMouseX_,lastMouseY_);
+              if (mouseOverArea_!=mouseOverNew) {
+                // end old:
+                if (!mouseDownCaptured_ && mouseDownArea_!=NULL && mouseDownArea_!=mouseOverNew) {
+                  mouseDownArea_->onMouseDownEnd(mouseDownCaptured_,false,lastMouseX_,lastMouseY_);
+                  mouseDownArea_=NULL;
+                }
+                if (mouseOverArea_!=NULL) {
+                  mouseOverArea_->onMouseOverEnd();
+                  mouseOverArea_ = NULL;
+                }
+                // start new:
+                if (mouseOverNew!=NULL) {
+                  mouseOverArea_ = mouseOverNew;
+                  mouseOverArea_->onMouseOverStart();
+
+                  if (mouseDown_ && !mouseDownArea_) {
+                    mouseDownArea_ = mouseOverArea_;
+                    mouseDownArea_->onMouseDownStart(false,lastMouseX_,lastMouseY_);
+                    mouseDownCaptured_ = false;
+                  }
+                }
+              }
+              break;
+            }
+            case sf::Event::Resized: {
+              sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+              renderWindow_->setView(sf::View(visibleArea));
+              sf::Vector2u size = renderWindow_->getSize();
+              mainArea_->sizeIs(size.x,size.y);
+              break;
+            }
+            default: {break;}
+          }
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
+          close();
+        }
+      }
+      void draw() {
+        renderWindow_->clear();
+        mainArea_->draw(0,0,*renderWindow_);
+        // DrawDot(MOUSE_X, MOUSE_Y, window, sf::Color(255*MOUSE_RIGHT_DOWN,255*MOUSE_LEFT_DOWN,255));
+        if (mouseOverArea_) {
+          EP::DrawText(5,5, "over: " + mouseOverArea_->fullName(), 10, *renderWindow_, EP::Color(1.0,1.0,1.0));
+        }
+        if(mouseDown_) {
+          std::string p = "down: " + (mouseDownArea_?mouseDownArea_->fullName():"None") + " " + (mouseDownCaptured_?"captured":"glide");
+          EP::DrawText(5,20, p, 10, *renderWindow_, EP::Color(1.0,1.0,1.0));
+        }
+        renderWindow_->display();
+      }
+      void close() {renderWindow_->close();}
+      bool isAlive() {return renderWindow_->isOpen();}
+    private:
+      sf::RenderWindow *renderWindow_;
+      Area* mainArea_;
+      Area* mouseOverArea_;
+      bool mouseDown_ = false;
+      Area* mouseDownArea_;
+      float mouseDownCaptured_ = false;
+      float lastMouseX_;
+      float lastMouseY_;
+    };
   }// namespace GUI
 }// namespace EP
 #endif //EP_GUI_HPP
--- 12 gui project/main.cpp
+++ 12 gui project/main.cpp
@@ -16,145 +16,150 @@
 
 #include "ep_gui.hpp"
 
+
 //-------------------------------------------------------- Main
 int main()
 {
-    // ------------------------------------ WINDOW
-    sf::ContextSettings settings;
-    settings.antialiasingLevel = 8;
-    //sf::Style style = sf::Style::Fullscreen;//Default
-    sf::RenderWindow window(sf::VideoMode(1000, 600), "first attempts", sf::Style::Default, settings);
+  // ------------------------------------ WINDOW
+  EP::GUI::MasterWindow* masterWindow = new EP::GUI::MasterWindow(1000,600,"window title",false);
+  {
+    EP::GUI::Window* window1 = (new EP::GUI::Window("window1",masterWindow->area(),200,100,200,300,"MyWindow"));
+    EP::GUI::Window* window2 = (new EP::GUI::Window("window2",masterWindow->area(),250,150,300,200,"MyWindow"));
+  }
+  while (masterWindow->isAlive()) {
+    masterWindow->update();
+    masterWindow->draw();
+  }
+}
 
-    window.setVerticalSyncEnabled(true);
-    //window.setMouseCursorVisible(false);
-
-    EP::GUI::Area* windowArea;
-    {
-      sf::Vector2u size = window.getSize();
-      windowArea = (new EP::GUI::Area("winArea",NULL,0,0,size.x,size.y))->fillParentIs(true);
-      EP::GUI::Window* window1 = (new EP::GUI::Window("window1",windowArea,200,100,200,300,"MyWindow"));
-      EP::GUI::Window* window2 = (new EP::GUI::Window("window2",windowArea,250,150,300,200,"MyWindow"));
-    }
+    // EP::GUI::Area* windowArea;
+    // {
+    //   sf::Vector2u size = window.getSize();
+    //   windowArea = (new EP::GUI::Area("winArea",NULL,0,0,size.x,size.y))->fillParentIs(true);
+    //   EP::GUI::Window* window1 = (new EP::GUI::Window("window1",windowArea,200,100,200,300,"MyWindow"));
+    //   EP::GUI::Window* window2 = (new EP::GUI::Window("window2",windowArea,250,150,300,200,"MyWindow"));
+    // }
 
     //--------------------------------------- Event states:
-    EP::GUI::Area* mouseOver = NULL;
+    //EP::GUI::Area* mouseOver = NULL;
 
     // ------------------------------------ FPS
-    clock_t last_stamp = clock();
-    clock_t deltaTime = 1000;
-    float framesPerSec = 10;
-    float framesPerSec_avg = 10;
-    float averageDelta = 1000;
-    size_t ticker = 0;
-
-    auto FPS_tick = [&last_stamp, &deltaTime, &framesPerSec, &framesPerSec_avg, &averageDelta, &ticker]()
-    {
-      clock_t newTime = clock();
-      deltaTime = newTime - last_stamp;
-
-      averageDelta = 0.01 * deltaTime + 0.99 * averageDelta;
-
-      framesPerSec = CLOCKS_PER_SEC / (float)deltaTime;
-      framesPerSec_avg = CLOCKS_PER_SEC / (float)averageDelta;
-
-      last_stamp = newTime;
-      ticker++;
-    };
+    // clock_t last_stamp = clock();
+    // clock_t deltaTime = 1000;
+    // float framesPerSec = 10;
+    // float framesPerSec_avg = 10;
+    // float averageDelta = 1000;
+    // size_t ticker = 0;
+    //
+    // auto FPS_tick = [&last_stamp, &deltaTime, &framesPerSec, &framesPerSec_avg, &averageDelta, &ticker]()
+    // {
+    //   clock_t newTime = clock();
+    //   deltaTime = newTime - last_stamp;
+    //
+    //   averageDelta = 0.01 * deltaTime + 0.99 * averageDelta;
+    //
+    //   framesPerSec = CLOCKS_PER_SEC / (float)deltaTime;
+    //   framesPerSec_avg = CLOCKS_PER_SEC / (float)averageDelta;
+    //
+    //   last_stamp = newTime;
+    //   ticker++;
+    // };
 
     // --------------------------- Render Loop
-    while (window.isOpen())
-    {
-        // MOUSE_LEFT_HIT = false;
-        // MOUSE_RIGHT_HIT = false;
-
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            switch (event.type)
-            {
-                // window closed
-                case sf::Event::Closed:
-                  window.close();
-                  break;
-                case sf::Event::MouseWheelScrolled:
-                  if (event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel)
-                  {
-                    // int delta = event.mouseWheelScroll.delta;
-                    // if(delta > 0)
-                    // {
-                    //   screen_zoom*= 1.1;
-                    // }else{
-                    //   screen_zoom*= 0.9;
-                    // }
-                  }
-                  break;
-                case sf::Event::MouseButtonPressed:
-                  // if(event.mouseButton.button == sf::Mouse::Left)
-                  // {
-                  //   MOUSE_LEFT_HIT = true;
-                  //   MOUSE_LEFT_DOWN = true;
-                  // }else if(event.mouseButton.button == sf::Mouse::Right)
-                  // {
-                  //   MOUSE_RIGHT_HIT = true;
-                  //   MOUSE_RIGHT_DOWN = true;
-                  // }
-                  break;
-                case sf::Event::MouseButtonReleased:
-                  // if(event.mouseButton.button == sf::Mouse::Left)
-                  // {
-                  //   MOUSE_LEFT_DOWN = false;
-                  // }else if(event.mouseButton.button == sf::Mouse::Right)
-                  // {
-                  //   MOUSE_RIGHT_DOWN = false;
-                  // }
-                  break;
-                case sf::Event::Resized:
-                  {
-                    // update the view to the new size of the window
-                    sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
-                    window.setView(sf::View(visibleArea));
-                    sf::Vector2u size = window.getSize();
-                    windowArea->sizeIs(size.x,size.y);
-                  }
-                  break;
-                // we don't process other types of events
-                default:
-                  break;
-            }
-        }
-
-        sf::Vector2i mousepos = sf::Mouse::getPosition(window);
-        // MOUSE_X = mousepos.x;
-        // MOUSE_Y = mousepos.y;
-        EP::GUI::Area* mouseOverNew = windowArea->checkMouseOver(mousepos.x,mousepos.y,1.0);
-        if (mouseOver!=mouseOverNew) {
-          if (mouseOver!=NULL) {mouseOver->onMouseOverEnd();}
-          if (mouseOverNew!=NULL) {mouseOverNew->onMouseOverStart();}
-          mouseOver = mouseOverNew;
-        }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
-          window.close();
-        }
-
-        window.clear();
-
-        windowArea->draw(0,0,1.0,window);
-
-        // DrawDot(MOUSE_X, MOUSE_Y, window, sf::Color(255*MOUSE_RIGHT_DOWN,255*MOUSE_LEFT_DOWN,255));
-
-        FPS_tick();
-
-        EP::DrawText(5,5, "FPS: " + std::to_string(framesPerSec), 10, window, EP::Color(1.0,1.0,1.0));
-        EP::DrawText(5,20, "avg: " + std::to_string(framesPerSec_avg), 10, window, EP::Color(1.0,1.0,1.0));
-        EP::DrawText(5,35, "ticker: " + std::to_string(ticker), 10, window, EP::Color(1.0,1.0,1.0));
-        if (mouseOver) {
-          EP::DrawText(5,60, "over: " + mouseOver->fullName(), 10, window, EP::Color(1.0,1.0,1.0));
-        }
-
-
-        window.display();
-    }
-
-    return 0;
-}
+//     while (window.isOpen())
+//     {
+//         // MOUSE_LEFT_HIT = false;
+//         // MOUSE_RIGHT_HIT = false;
+//
+//         sf::Event event;
+//         while (window.pollEvent(event))
+//         {
+//             switch (event.type)
+//             {
+//                 // window closed
+//                 case sf::Event::Closed:
+//                   window.close();
+//                   break;
+//                 case sf::Event::MouseWheelScrolled:
+//                   if (event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel)
+//                   {
+//                     // int delta = event.mouseWheelScroll.delta;
+//                     // if(delta > 0)
+//                     // {
+//                     //   screen_zoom*= 1.1;
+//                     // }else{
+//                     //   screen_zoom*= 0.9;
+//                     // }
+//                   }
+//                   break;
+//                 case sf::Event::MouseButtonPressed:
+//                   // if(event.mouseButton.button == sf::Mouse::Left)
+//                   // {
+//                   //   MOUSE_LEFT_HIT = true;
+//                   //   MOUSE_LEFT_DOWN = true;
+//                   // }else if(event.mouseButton.button == sf::Mouse::Right)
+//                   // {
+//                   //   MOUSE_RIGHT_HIT = true;
+//                   //   MOUSE_RIGHT_DOWN = true;
+//                   // }
+//                   break;
+//                 case sf::Event::MouseButtonReleased:
+//                   // if(event.mouseButton.button == sf::Mouse::Left)
+//                   // {
+//                   //   MOUSE_LEFT_DOWN = false;
+//                   // }else if(event.mouseButton.button == sf::Mouse::Right)
+//                   // {
+//                   //   MOUSE_RIGHT_DOWN = false;
+//                   // }
+//                   break;
+//                 case sf::Event::Resized:
+//                   {
+//                     // update the view to the new size of the window
+//                     sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+//                     window.setView(sf::View(visibleArea));
+//                     sf::Vector2u size = window.getSize();
+//                     windowArea->sizeIs(size.x,size.y);
+//                   }
+//                   break;
+//                 // we don't process other types of events
+//                 default:
+//                   break;
+//             }
+//         }
+//
+//         sf::Vector2i mousepos = sf::Mouse::getPosition(window);
+//         // MOUSE_X = mousepos.x;
+//         // MOUSE_Y = mousepos.y;
+//         EP::GUI::Area* mouseOverNew = windowArea->checkMouseOver(mousepos.x,mousepos.y,1.0);
+//         if (mouseOver!=mouseOverNew) {
+//           if (mouseOver!=NULL) {mouseOver->onMouseOverEnd();}
+//           if (mouseOverNew!=NULL) {mouseOverNew->onMouseOverStart();}
+//           mouseOver = mouseOverNew;
+//         }
+//
+//         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
+//           window.close();
+//         }
+//
+//
+//         window.clear();
+//
+//         windowArea->draw(0,0,window);
+//
+//         // DrawDot(MOUSE_X, MOUSE_Y, window, sf::Color(255*MOUSE_RIGHT_DOWN,255*MOUSE_LEFT_DOWN,255));
+//
+//         FPS_tick();
+//
+//         EP::DrawText(5,5, "FPS: " + std::to_string(framesPerSec), 10, window, EP::Color(1.0,1.0,1.0));
+//         EP::DrawText(5,20, "avg: " + std::to_string(framesPerSec_avg), 10, window, EP::Color(1.0,1.0,1.0));
+//         EP::DrawText(5,35, "ticker: " + std::to_string(ticker), 10, window, EP::Color(1.0,1.0,1.0));
+//         if (mouseOver) {
+//           EP::DrawText(5,60, "over: " + mouseOver->fullName(), 10, window, EP::Color(1.0,1.0,1.0));
+//         }
+//
+//
+//         window.display();
+//     }
+//
+//     return 0;
+// }
