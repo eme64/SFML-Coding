165d2895bc8b16e93046eed5dd43f3bd153b6b99
Emanuel Peter <emanuel.peter@hotmail.com>
2020-02-09 22:29:20 +0100

    13 NC / voronoi game: add rounds

--- 13 NetController/voronoi_room.hpp
+++ 13 NetController/voronoi_room.hpp
@@ -30,7 +30,7 @@ private:
 
 struct DetailNoise {
    std::vector<FastNoise> noiseGen;
-   DetailNoise(int &seed_, float baseFreq = 0.007, int depth=6) {
+   DetailNoise(int &seed_, float baseFreq = 0.007, int depth=5) {
       noiseGen.resize(depth);
       for(int i=0; i<noiseGen.size(); i++) {
 	 noiseGen[i].SetNoiseType(FastNoise::SimplexFractal);
@@ -48,7 +48,7 @@ struct DetailNoise {
 };
 
 struct CInfo {
-   enum Type {Normal, Blocked, Fast, Slow};
+   enum Type {Normal, Blocked, Fast, Slow, Goal};
    Type t;
 };
 
@@ -56,41 +56,60 @@ class VoronoiRoom : public evp::Room {
 public:
    VoronoiRoom(const std::string &name, evp::RoomServer* server,float dx, float dy)
    : evp::Room(name,server), dx_(dx), dy_(dy) {
-      vmap = new evp::VoronoiMap<CInfo>(5000, dx_-10,dy_-10);
+      vmap = new evp::VoronoiMap<CInfo>(3000, dx_-10,dy_-10);
       
       seed_=0;
       setupMap();
+      setupUsers();
    }
    
    void setupMap() {
-      DetailNoise n0(seed_);// for types
-      DetailNoise n1(seed_);// for coloring?
+      DetailNoise n0(seed_);
+      DetailNoise n1(seed_);
+      DetailNoise n2(seed_);
          
       for(int i=0; i<vmap->num_cells; i++) {
 	 float x = vmap->cells[i].pos.x;
 	 float y = vmap->cells[i].pos.y;
-	 float h0 = n0.get(x,y);
+	 float h0 = n0.get(x,y)*(x<100?x*0.01:1.0)*((x>dx_-100)?(1.0-(x-dx_+100)*0.01):1.0);
 	 float h1 = n1.get(x,y);
+	 float h2 = n2.get(x,y)*(x<100?x*0.01:1.0)*((x>dx_-100)?(1.0-(x-dx_+100)*0.01):1.0);
          
 	 Color c(h0,h0,h0);
-	 if(h0 > 0.6) {
+	 if(h0 > 0.57) {
 	    vmap->cells[i].info.t = CInfo::Blocked;
 	 } else {
-            if(h1 > 0.6) {
+            if(h2 > 0.55) {
+	       vmap->cells[i].info.t = CInfo::Slow;
+	       c = Color((1-h1)*0.7,h0*0.3,h0*0.3);
+	    } else if(h1>0.58) {
 	       vmap->cells[i].info.t = CInfo::Fast;
 	       c = Color(h0*0.5,h1,h0*0.5);
-	    } else if(h1<0.3) {
-	       vmap->cells[i].info.t = CInfo::Slow;
-	       c = Color(1-h1,h0*0.5,h0*0.5);
+	       c = Color(h0*0.3,h1*0.7,h0*0.3);
 	    } else {
 	       vmap->cells[i].info.t = CInfo::Normal;
-	       c = Color(h0*0.5,h0*0.5,h0*0.5);
+	       c = Color(h0*0.2,h0*0.2,h0*0.2);
 	    }
 	 }
 	 vmap->cells[i].color = c.toSFML();
       }
 
+      auto &goal = vmap->cells[vmap->getCell(dx_-50,dy_*0.5,0)];
+      goal.info.t = CInfo::Goal;
+      goal.color = Color(1,1,0).toSFML();
+
       vmap->create_mesh();
+   }
+   void setupUsers() {
+      evp::Room::UserVisitF freset = [&] (evp::User* user, evp::UserData* raw) {
+         VoronoiUserData* data = dynamic_cast<VoronoiUserData*>(raw);
+         data->x = 50;
+         data->y = dy_*0.5;
+         data->dx = 0;
+         data->dy = 0;
+         data->w = 0;
+      };
+      visitUsers(freset);
    }
 
    ~VoronoiRoom() {
@@ -98,6 +117,12 @@ public:
    }
 
    virtual void draw(sf::RenderTarget &target) {
+      if(winner_) {
+         winner_=NULL;
+	 setupMap();
+         setupUsers();
+      }
+      
       vmap->draw(0,0,1,target);
       evp::Room::UserVisitF f = [&] (evp::User* user, evp::UserData* raw) {
          VoronoiUserData* data = dynamic_cast<VoronoiUserData*>(raw);
@@ -109,8 +134,8 @@ public:
 	 data->dy += dyy*0.01;
          data->x += data->dx;
          data->y += data->dy;
-	 data->dx *= 0.99;
-	 data->dy *= 0.99;
+	 data->dx *= 0.98;
+	 data->dy *= 0.98;
 	 
 	 if(data->x < 10) {data->x = 10;data->dx=0;}
 	 if(data->y < 10) {data->y = 10;data->dy=0;}
@@ -121,7 +146,9 @@ public:
          auto &cell = vmap->cells[cellI];
          
 	 Color c(0,1,0);
-	 if(cell.info.t == CInfo::Blocked) {
+	 if(cell.info.t == CInfo::Goal) {
+	    setWinner(user);
+	 } else if(cell.info.t == CInfo::Blocked) {
 	    c = Color(1,0,0);
 	    float ddx = data->x - cell.pos.x;
 	    float ddy = data->y - cell.pos.y;
@@ -135,6 +162,8 @@ public:
 	    data->dx *= 0.8;
 	    data->dy *= 0.8;
 	 }
+         DrawRect(data->x-3,data->y-3, 6,6, target, Color(0,0,0));
+         DrawRect(data->x-2 + 5*dxx,data->y-2 + 5*dyy, 4,4, target, Color(0,0,0));
          DrawRect(data->x-2,data->y-2, 4,4, target, c);
          DrawRect(data->x-1 + 5*dxx,data->y-1 + 5*dyy, 2,2, target, c);
       };
@@ -166,11 +195,13 @@ public:
 				 if(down) {this->server()->setActive("lobby");}
 			      }));
    }
+   void setWinner(evp::User* w) {winner_ = w;}
 private:
    evp::VoronoiMap<CInfo> *vmap;
    int seed_;
    float dx_;
    float dy_;
+   evp::User* winner_=NULL;
 };
 
 
