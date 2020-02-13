c80a20c17247e7040cfe80f4b8855546fa394490
Emanuel Peter <emanuel.peter@hotmail.com>
2020-02-13 15:23:48 +0100

    13 NC / separate rocket_room.hpp, introduce CellMap

--- 13 NetController/evp_voronoi.hpp
+++ 13 NetController/evp_voronoi.hpp
@@ -30,12 +30,14 @@ void voronoi_generate(int numpoints, const jcv_point* points, jcv_diagram &diagr
   jcv_diagram_generate(numpoints, points, rect, &diagram);
 }
 
-void voronoi_relax_points(const jcv_diagram &diagram, jcv_point* points, _jcv_rect* rect)
+void voronoi_relax_points(const jcv_diagram &diagram, jcv_point* points, _jcv_rect* rect,
+		          const std::vector<bool> &pointsFix)
 {
   const jcv_site* sites = jcv_diagram_get_sites(&diagram);
   for( int i = 0; i < diagram.numsites; ++i )
   {
     const jcv_site* site = &sites[i];
+    if(pointsFix[site->index]) {continue;}
     jcv_point sum = site->p;
     int count = 1;
 
@@ -105,10 +107,309 @@ void voronoi_free(jcv_diagram &diagram)
 
 // ---------------------------------------------------------- USEFUL OBJECTS
 
+// Predefined structures / points:
+// set points + exclusion zone for random points
+// points / cells refer back to object, call its functions?
+//
+// allow generation of structured cell maps - no voronoi ?
+
 struct Point {
    float x,y;
    Point(float x=0, float y=0) : x(x),y(y) {}
 };
+
+struct CellMapSubstructure {
+   std::vector<Point> points;
+   virtual bool isInside(float x, float y) {return false;}
+};
+
+struct CellMapSubstructureGrid : public CellMapSubstructure {
+   float x0,y0,x1,y1;
+   CellMapSubstructureGrid(int nx, int ny, float x0, float y0, float x1, float y1) 
+   : x0(x0),y0(y0),x1(x1),y1(y1) {
+      float dx = x1-x0; float dy = y1-y0;
+      points.reserve(nx*ny);
+      for(int xx=0; xx<nx; xx++) {
+	 float x = x0 + dx*(xx+0.5)/nx;
+         for(int yy=0; yy<ny; yy++) {
+	    float y = y0 + dy*(yy+0.5)/ny;
+	    points.push_back(Point(x,y));
+	 }
+      }
+   }
+   virtual bool isInside(float x, float y) {
+      return x>=x0 and x<x1 and y>=y0 and y<y1;
+   }
+};
+
+
+template <typename CInfo>
+struct CellMapCell
+{
+  Point pos;
+  sf::Color color;
+
+  std::vector<Point> corners;
+  std::vector<int> neighbors;
+
+  CInfo info;
+};
+
+template <typename CInfo>
+struct CellMap
+{
+  /*
+   --- this is basically a graph
+   V : the center of each polygon/cell (contains cell and edges)
+   E : connections between cells
+  */
+
+  const float x0,y0,x1,y1;
+  size_t numCells;
+  std::vector<CellMapCell<CInfo>> cells;
+  std::vector<CellMapSubstructure*> subs;
+
+  std::vector<sf::Vertex> mesh;
+
+  CellMap(float x0, float y0, float x1, float y1,
+          const std::vector<CellMapSubstructure*> &subs,
+	  int nAddPoints)
+  : x0(x0),y0(y0),x1(x1),y1(y1), subs(subs)
+  {
+    // --------------------------- Prepare Points
+    numCells = nAddPoints;
+    for(CellMapSubstructure* s : subs) {
+       numCells += s->points.size();
+    }
+    jcv_point* points = NULL;
+    points = (jcv_point*)malloc( sizeof(jcv_point) * (size_t)numCells);
+    std::vector<bool> pointsFix(numCells,false);
+    // --------------------------- Fill in Points from subs
+    int pIndex=0;
+    for(CellMapSubstructure* s : subs) {
+       for(const Point& p : s->points) {
+          int i = pIndex++;
+	  points[i].x = p.x;
+          points[i].y = p.y;
+	  pointsFix[i] = true;
+       }
+    }
+    
+    // --------------------------- Fill in Additional Points
+    while(nAddPoints>0) {
+       float x = x0 + (x1-x0)*((float) rand() / (RAND_MAX));
+       float y = y0 + (y1-y0)*((float) rand() / (RAND_MAX));
+       bool hit = false;
+       for(CellMapSubstructure* s : subs) {
+          if(s->isInside(x,y)) {hit=true; break;}
+       }
+       if(!hit) {
+          nAddPoints--;
+	  int i = pIndex++;
+	  points[i].x = x;
+          points[i].y = y;
+       }
+    }
+
+    // --------------------------- Prepare Diagram
+    jcv_diagram diagram;
+    _jcv_rect rect;
+    rect.min.x = x0;
+    rect.min.y = y0;
+    rect.max.x = x1;
+    rect.max.y = y1;
+    voronoi_generate(numCells, points, diagram, &rect);
+
+    for(int i=0; i<10; i++) // relax some -> make more equispaced
+    {
+      voronoi_relax_points(diagram, points, &rect, pointsFix);
+      voronoi_free(diagram);
+      voronoi_generate(numCells, points, diagram, &rect);
+    }
+
+    // ----------------  extract graph
+    cells.resize(numCells);
+
+    for(size_t i = 0; i<numCells; i++)
+    {
+      cells[i].pos.x = points[i].x;
+      cells[i].pos.y = points[i].y;
+      cells[i].color = Color(
+		      (11*i % 256) / 256.0,
+		      (13*i % 256) / 256.0,
+		      (17*i % 256) / 256.0 ).toSFML();
+    }
+
+    // get neighbours and edges
+    const jcv_site* sites = jcv_diagram_get_sites( &diagram );
+    for( int i = 0; i < diagram.numsites; ++i )
+    {
+      const jcv_site* site = &sites[i];
+
+      int index = site->index;
+
+      const jcv_graphedge* e = site->edges;
+      while( e )
+      {
+        cells[index].corners.push_back({e->pos[0].x, e->pos[0].y});
+        if(e->neighbor && e->neighbor->index >= 0 && e->neighbor->index < numCells){
+          cells[index].neighbors.push_back(e->neighbor->index);
+        }
+
+        e = e->next;
+      }
+    }
+
+    voronoi_free(diagram);
+
+    create_mesh();
+  }
+  void create_mesh()
+  {
+    mesh.clear();
+
+    for(size_t i = 0; i<numCells; i++)
+    {
+      int nc = cells[i].corners.size();
+
+      for(int c = 2; c<nc; c++){
+        mesh.push_back(sf::Vertex(
+                          sf::Vector2f(cells[i].corners[0].x,cells[i].corners[0].y),
+                          cells[i].color));
+
+        mesh.push_back(sf::Vertex(
+                          sf::Vector2f(cells[i].corners[c-1].x,cells[i].corners[c-1].y),
+                          cells[i].color));
+
+        mesh.push_back(sf::Vertex(
+                          sf::Vector2f(cells[i].corners[c].x,cells[i].corners[c].y),
+                          cells[i].color));
+      }
+    }
+  }
+
+  size_t getCell(float x, float y, size_t guess_cell)
+  {
+    size_t number_iterations = 0;
+    // supposed to speed up, because could be close:
+    size_t res = guess_cell;
+
+    float dx = cells[guess_cell].pos.x - x;
+    float dy = cells[guess_cell].pos.y - y;
+    float squaredist = dx*dx + dy*dy; // from initial guess
+
+    bool done = false;
+    while(!done)
+    {
+      done = true;
+
+      // check neighbors of current guess, find better fit
+      size_t nc = cells[res].neighbors.size();
+
+      for(int i=0; i<nc; i++)
+      {
+        size_t n = cells[res].neighbors[i];
+        dx = cells[n].pos.x - x;
+        dy = cells[n].pos.y - y;
+
+        float d = dx*dx + dy*dy;
+
+        if(d < squaredist)
+        {
+          res = n;
+          squaredist = d;
+          done = false;
+          number_iterations++;
+          break; // needed to recompute nc !
+        }
+      }
+    }
+    return res;
+  }
+
+  //size_t getCell(float x, float y)
+  //{
+  //  size_t res = 0;
+  //  float squaredist = spread_x*spread_x + spread_y*spread_y; // max dist on map !
+
+  //  for(size_t i = 0; i<num_cells; i++)
+  //  {
+  //    float dx = cells[i].pos.x - x;
+  //    float dy = cells[i].pos.y - y;
+
+  //    float d = dx*dx + dy*dy;
+
+  //    if(d < squaredist)
+  //    {
+  //      res = i;
+  //      squaredist = d;
+  //    }
+  //  }
+
+  //  return res;
+  //}
+
+  //void bfs(size_t root, std::vector<size_t> &parents)
+  //{
+  //  if(! cells[root].isLand)
+  //  {
+  //    // root is not reachable! -> make that clear in parents!
+  //    parents[root] = root;
+
+  //    size_t nn = cells[root].neighbors.size();
+  //    for(size_t nec = 0; nec<nn; nec++)
+  //    {
+  //      size_t ne = cells[root].neighbors[nec];
+  //      parents[ne] = ne;
+  //    }
+  //    return; // early abort
+  //  }
+
+  //  // creates a rooted tree from a bfs traversal
+  //  bool marked[num_cells] = {false};
+  //  std::queue<size_t> q;
+
+  //  parents[root] = root;
+  //  marked[root] = true;
+  //  q.push(root);
+
+  //  while(!q.empty())
+  //  {
+  //    size_t c = q.front();
+  //    q.pop();
+
+  //    marked[c] = true;
+
+  //    //push all children
+  //    size_t nn = cells[c].neighbors.size();
+  //    for(size_t nec = 0; nec<nn; nec++)
+  //    {
+  //      size_t ne = cells[c].neighbors[nec];
+  //      if(!marked[ne] && cells[ne].isLand)
+  //      {
+  //        parents[ne] = c;
+  //        q.push(ne);
+
+  //        //if(((float) rand() / (RAND_MAX)) < 0.9){ // makes it inprecize, helps them find different paths
+  //          marked[ne] = true;
+  //        //}
+  //      }
+  //    }
+  //  }
+  //}
+
+  void draw(float x, float y, float zoom, sf::RenderTarget &target)
+  {
+    sf::Transform t;
+    t.scale(zoom, zoom);
+    t.translate(x, y);
+
+    target.draw(&mesh[0], mesh.size(), sf::Triangles, t);
+  }
+};
+
+
+
 
 template <typename CInfo>
 struct VoronoiMapCell
@@ -147,6 +448,7 @@ struct VoronoiMap
     // --------------------------- Prepare Points
     jcv_point* points = NULL;
     points = (jcv_point*)malloc( sizeof(jcv_point) * (size_t)num_cells);
+    std::vector<bool> pointsFix(num_cells,false);
 
     for(size_t i = 0; i<num_cells; i++)
     {
@@ -165,7 +467,7 @@ struct VoronoiMap
 
     for(int i=0; i<10; i++) // relax some -> make more equispaced
     {
-      voronoi_relax_points(diagram, points, &rect);
+      voronoi_relax_points(diagram, points, &rect, pointsFix);
       voronoi_free(diagram);
       voronoi_generate(num_cells, points, diagram, &rect);
     }
--- 13 NetController/main.cpp
+++ 13 NetController/main.cpp
@@ -12,9 +12,10 @@
 #include <streambuf>
 
 #include "evp_server.hpp"
+#include "evp_draw.hpp"
 
 #include "voronoi_room.hpp"
-#include "evp_draw.hpp"
+#include "rocket_room.hpp"
 
 // Ideas:
 //  - file adapter
@@ -61,18 +62,15 @@
 class LobbyRoom : public evp::Room {
 public:
    LobbyRoom(const std::string &name, evp::RoomServer* server)
-   : evp::Room(name,server) {}
+   : evp::Room(name,server) {
+      std::vector<evp::CellMapSubstructure*> subs;
+      subs.push_back(new evp::CellMapSubstructureGrid(10,10, 200,200,300,300));
+      subs.push_back(new evp::CellMapSubstructureGrid(10,10, 350,200,450,300));
+      cm_ = new evp::CellMap<int>(10,10,790,590, subs, 4000);
+   }
    virtual void draw(sf::RenderTarget &target) {
       DrawRect(10,10, 100,100, target, Color(0.5,0.5,0));
-      //evp::Room::UserVisitF f = [&] (evp::User* user, evp::UserData* raw) {
-      //   MyUserData* data = dynamic_cast<MyUserData*>(raw);
-      //   if(data->down) {
-      //      DrawRect(data->x,data->y, 5,5, target, Color(0,1,0));
-      //   } else {
-      //      DrawRect(data->x,data->y, 5,5, target, Color(1,0,0));
-      //   }
-      //};
-      //visitUsers(f);
+      cm_->draw(0,0,1,target);
    }
    virtual void onActivate() {
       std::cout << "MyActivate " << name() << std::endl;
@@ -95,6 +93,7 @@ public:
 			      }));
    }
 private:
+   evp::CellMap<int>* cm_;
 };
 
 
@@ -152,133 +151,6 @@ public:
 private:
 };
 
-class Particle {
-public:
-   Particle(float x,float y, float dx, float dy, float w, float dw, float s, const Color &color, int t)
-   : x(x),y(y),dx(dx),dy(dy), w(w),dw(dw), s(s), color(color), t0(t),t(t) {}
-   bool draw(sf::RenderTarget &target) {
-      w+=dw;
-      x+=dx;
-      y+=dy;
-      DrawRect(x, y, s,s, target, Color(color.r, color.g, color.b, ((float) t) / ((float) t0)), w);
-      return (t--)>0;
-   }
-private:
-   float x,y,dx,dy,w,dw,s;
-   int t0,t;
-   Color color;
-};
-
-class RocketUserData : public evp::UserData {
-public:
-   float x=100, y=100;
-   float dx=0, dy=0;
-   float w = 90;
-   bool up=false, left=false, right=false;
-private:
-};
-
-class RocketRoom : public evp::Room {
-public:
-   RocketRoom(const std::string &name, evp::RoomServer* server)
-   : evp::Room(name,server) {
-      sf::ConvexShape polygon;
-      polygon.setPointCount(5);
-      polygon.setPoint(0, sf::Vector2f(1, 0));
-      polygon.setPoint(1, sf::Vector2f(0.7, 0.2));
-      polygon.setPoint(2, sf::Vector2f(-1, 0.2));
-      polygon.setPoint(3, sf::Vector2f(-1, -0.2));
-      polygon.setPoint(4, sf::Vector2f(0.7, -0.2));
-      rocket_.push_back(polygon);
-
-      x0_=20;
-      y0_=20;
-      x1_=780;
-      y1_=580;
-   }
-   virtual void draw(sf::RenderTarget &target) {
-      DrawRect(x0_,y0_, x1_-x0_, y1_-y0_, target, Color(0.1,0.1,0.1));
-      evp::Room::UserVisitF f = [&] (evp::User* user, evp::UserData* raw) {
-         RocketUserData* data = dynamic_cast<RocketUserData*>(raw);
-	 
-	 if(data->left) {data->w-=5;}
-	 if(data->right) {data->w+=5;}
-         
-	 if(data->up) {
-            const float dxx = std::cos(data->w / 360.0 * 2.0 * M_PI);
-            const float dyy = std::sin(data->w / 360.0 * 2.0 * M_PI);
-            data->dx+=0.1*dxx;
-            data->dy+=0.1*dyy;
-
-	    particles_.insert(new Particle(data->x, data->y, data->dx-dxx*2, data->dy-dyy*2, 0,0, 3, Color(1,0.5,0.2), 60));
-	 }
-         
-	 data->dy+=0.01;
-	 data->x += data->dx;
-	 data->y += data->dy;
-	 data->dx*=0.99;
-	 data->dy*=0.99;
-	 
-         if(data->x < x0_) {data->x = x0_; data->dx = 0;}
-         if(data->x > x1_) {data->x = x1_; data->dx = 0;}
-         if(data->y < y0_) {data->y = y0_; data->dy = 0;}
-         if(data->y > y1_) {data->y = y1_; data->dy = 0;}
-         
-	 drawRocket(data->x,data->y, 10, data->w, target, Color(0.5 + data->left,0.5 + data->right, 0.5 + data->up));
-      };
-      visitUsers(f);
-      
-      for(auto it = particles_.begin(); it!=particles_.end(); it++) {
-         Particle *p = *it;
-	 if(! p->draw(target)) {
-	    delete p;
-	    particles_.erase(it);
-	 }
-      }
-   }
-   virtual evp::UserData* newUserData(const evp::User* u) { return new RocketUserData();}
-   virtual void onActivate() {
-      std::cout << "RocketActivate " << name() << std::endl;
-   }
-   virtual void onActivateUser(evp::User* const u,evp::UserData* const raw) {
-      RocketUserData* data = dynamic_cast<RocketUserData*>(raw);
-      std::cout << "RocketActivateUser " << name() << " " << u->name() << std::endl;
-   
-      u->clearControls();
-      u->registerControl(new evp::ButtonControl(u->nextControlId(),0.05,0.05,0.9,0.2,"Escape",
-			      [this](bool down) {
-				 if(down) {this->server()->setActive("lobby");}
-			      }));
-      u->registerControl(new evp::ButtonControl(u->nextControlId(),0,0.4,0.3,0.5,"KeyA",
-			      [data](bool down) {
-				 data->left = down;
-			      }));
-      u->registerControl(new evp::ButtonControl(u->nextControlId(),0.35,0.3,0.3,0.5,"KeyW",
-			      [data](bool down) {
-				 data->up = down;
-			      }));
-      u->registerControl(new evp::ButtonControl(u->nextControlId(),0.7,0.4,0.3,0.5,"KeyD",
-			      [data](bool down) {
-				 data->right = down;
-			      }));
-   }
-private:
-   float x0_,y0_,x1_,y1_; // limits of room
-   std::set<Particle*> particles_;
-   std::vector<sf::ConvexShape> rocket_;
-
-   void drawRocket(float x,float y, float scale, float w, sf::RenderTarget &target, const Color& color) {
-      for(sf::Shape &s : rocket_) {
-         s.setFillColor(color.toSFML());
-         s.setPosition(x,y);
-	 s.setScale(scale,scale);
-	 s.setRotation(w);
-	 target.draw(s, sf::BlendAlpha);
-      }
-   }
-};
-
-
 
 int main(int argc, char** argv) {
    std::cout << "Starting..." << std::endl;
@@ -329,8 +201,8 @@ int main(int argc, char** argv) {
       // --------------- DRAW
       window->clear();
       
-      DrawRect(mouseX, mouseY, 5,5, *window, Color(1,1,1));
       server.draw(*window);
+      DrawRect(mouseX, mouseY, 5,5, *window, Color(1,1,1));
       
       window->display();
    }
